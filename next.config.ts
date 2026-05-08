dd999e0d127f3db5794ffb01bb313740143d0384
Ajay Sharma <ajay123@gmail.com>
2026-05-08 14:23:27 +0530

    layout globalss

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "randomuser.me",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
